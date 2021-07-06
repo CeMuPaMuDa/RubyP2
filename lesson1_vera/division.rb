15f88c566f18cb5be462ebcd8b75495e4477fde6
CeMuPaMuDa <bepyc9i@gmail.com>
2021-07-06 09:24:10 +0300

    fix

--- lesson1_vera/division.rb
+++ lesson1_vera/division.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-require_relative 'lib\mathem_op'
+require_relative 'lib/mathem_op'
 
 print 'Введите перове число: '
 a = gets.to_f
+b = ''
 loop do
   print 'Введите второе число (кроме нуля): '
   b = gets.to_f
   break unless b.zero? || b.nil?
 end
+
 
 puts "Результат деления: #{MathemathicsOperations.new.division(a, b)}"
