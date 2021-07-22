77b5a05fafb2253402eb22d736bc0bca8e0e034b
CeMuPaMuDa <bepyc9i@gmail.com>
2021-07-22 15:46:40 +0300

    fix2

--- lesson6_vera/2_arr_to_h.rb
+++ lesson6_vera/2_arr_to_h.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 arr = %w[first second third]
-puts(arr.each_with_index
+puts(arr.map(&:to_sym).each_with_index
         .to_h { |x, i| [x, x[0].upcase + x[-2, 2] + "(#{i + 1})"] })
