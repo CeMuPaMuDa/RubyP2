5559dc332d31d45c60105a20a9c4f4f3c60eab0b
CeMuPaMuDa <bepyc9i@gmail.com>
2021-07-22 03:43:53 +0300

    rubocop

--- lesson5_vera/lib/list.rb
+++ lesson5_vera/lib/list.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class List
- def initialize(*params)
+  def initialize(*params)
     @params = params
   end
 
   def each(&block)
     @params.each(&block)
   end
-  end
+end
